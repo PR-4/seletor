ab52a62c4097777aca7581fe79cd6aa24910e6b6
Victor Resproj <victorcarreira@id.uff.br>
2023-04-25 18:45:35 -0300

    programa seletor de lista de poços

---
+++ program/seletor.py
@@ -0,0 +1,30 @@
+#Seletor
+#programa que seleciona uma lista de IDs de poços e armazena em um dataframe
+
+#Pacotes
+import numpy as np
+import pandas as pd
+import time as t
+
+
+
+ini = t.time()
+
+# leitura da tabela da ANP
+ANP = pd.read_excel(open('../input/tabela-de-pocos.xlsx', 'rb'),index_col=0 ,sheet_name='Planilha1',header=0)
+
+# leitura do arquivo de seleção
+lista = ['3-FQ-21-ES','7-CAM-114-RN','4-LD-7-ES']
+
+# seletor
+seletor = ANP.loc[lista]
+
+print(seletor)
+
+
+#saida
+seletor.to_excel('../output/selecao.xlsx', index=False, header=True )
+
+fim = t.time()
+print('Tempo de processamento=',(fim-ini)/60,'minutos')
+
