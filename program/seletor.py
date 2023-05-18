cd726fffe333d53c8b07957d38544c2bb106f14b
Victor Resproj <victorcarreira@id.uff.br>
2023-05-18 13:36:59 -0300

    Adiciona o programa seletor

--- program/seletor.py
+++ program/seletor.py
@@ -14,11 +14,12 @@ ini = t.time()
 ANP = pd.read_excel(open('../input/tabela-de-pocos.xlsx', 'rb') ,index_col= 0, sheet_name='Planilha1',header=0)
 
 # leitura do arquivo de seleção
-lista = ['3-FQ-21-ES','7-CAM-114-RN','4-LD-7-ES']
+lista = pd.read_excel(open('../input/controle_de_entrega_de_dados_v2.xlsx', 'rb'), sheet_name="Lara (11301 e 11302)",header=0)
+#lista = ['3-FQ-21-ES','7-CAM-114-RN','4-LD-7-ES']
 
 # seletor
 
-seletor = ANP.loc[lista]
+seletor = ANP.loc[lista['POCO'].to_list()]
 seletor = seletor.reset_index()
 print(seletor)
 
